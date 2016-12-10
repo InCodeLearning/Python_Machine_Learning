e2dba9abbd46463ea5457db6f92bb77d1479263e
Zijuan Zhang <zijuanzhang@gmail.com>
2016-12-10 10:35:02 -0600

    make pandas_series.py print clear and add pandas dataframe.py file

--- pandas_Series.py
+++ pandas_Series.py
@@ -12,31 +12,37 @@ import numpy as np
 x = [_ for _ in range(100, 105, 1)]
 sr1 = pd.Series(x, name="ID",
                 index=['a', 'b', 'c', 'd', 'e'])
+print('-----sr1-----')
 print(sr1)
 
 # method 2: create from dict
 d = {'a': 0, 'b': 1, 'c': 2, 'd': 3.0}
 sr2 = pd.Series(d)
+print('-----sr2-----')
 print(sr2)
 
 #  data type changed
 d = {'a': 0, 'b': 1, 'c': 'c', 'd': 3.0}
 sr3 = pd.Series(d)
+print('-----sr3-----')
 print(sr3)
 
 # NaN is standard missing data marker in pandas
 # pay attention to the order
 sr4 = pd.Series(d, index=['a', 'b', 'c', 'e', 'd'])
+print('-----sr4-----')
 print(sr4)
 
 # from scalar value, index must be provided to tell the length
 sr5 = pd.Series(2, index=['a', 'b', 'c'], name='scalar')
+print('-----sr5-----')
 print(sr5)
 
 # ndarray-like and dict-like, it is mutable,
 # and can be manipulated like dict and ndarray
 # int type can not manipulate sqrt, exp !!!!
 sr3['c'] = 2
+print('-----changed sr3-----')
 print(sr3)
 print(sr3[1])
 print(sr3+sr3)
@@ -46,9 +52,13 @@ print(sr3[sr3 >= 2])
 print(sr3)
 sr3['e'] = 4.0
 sr3 = pd.Series(sr3, dtype=float, name='example')
+print('---sr3 add item and name, change type---')
 print(sr3)
 print(np.sqrt(sr3))
 print(sr3.name)
 sr6 = sr3.rename("changed")  # after pandas v 18
+print('-----sr6-----')
 print(sr6)
+print(sr6.name)
+print(sr3)
 print(pd.__version__)
